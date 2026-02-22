e0fb234bef83764d99062e291c2ec56cb2083bc2
neveshardd <118855875+neveshardd@users.noreply.github.com>
2026-02-21 21:23:35 -0300

    iuniwa bud uawbd liABWLid BW

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,9 +1,20 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { PrivateLayoutClient } from "@/components/private-layout-client";
 
-export default function PrivateLayout({
+export default async function PrivateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth/signin");
+  }
+
   return <PrivateLayoutClient>{children}</PrivateLayoutClient>;
 }
